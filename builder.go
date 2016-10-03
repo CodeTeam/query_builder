e23ce97af81338e13a06d73e9093f74457a6daf8
Максим Шаламов <MVShalamov@sberned.ru>
2016-10-03 14:00:04 +0300

    del 'where' where empty wherecond

--- builder.go
+++ builder.go
@@ -1,28 +1,30 @@
 package builder
 
 import (
-    "strings"
-    "bytes"
+	"bytes"
+	"strings"
 )
 
 func (query *Query) BuildQuery() string {
-    var buffer bytes.Buffer
-    buffer.WriteString(query.TypeQuery)
-    buffer.WriteString(" ")
-    columns := interfaceToString(query.Columns)
-    buffer.WriteString(strings.Join(columns[:], ", "))
+	var buffer bytes.Buffer
+	buffer.WriteString(query.TypeQuery)
+	buffer.WriteString(" ")
+	columns := interfaceToString(query.Columns)
+	buffer.WriteString(strings.Join(columns[:], ", "))
 
-    buffer.WriteString(" From ")
-    buffer.WriteString(query.TableName)
+	buffer.WriteString(" From ")
+	buffer.WriteString(query.TableName)
 
-    buffer.WriteString(" Where ")
-    for index, element := range query.WhereCond {
-        if index == 0 {
-            buffer.WriteString(convertValueToString(element.Expression, element.Value))
-        } else {
-            buffer.WriteString(element.Delimiter)
-            buffer.WriteString(convertValueToString(element.Expression, element.Value))
-        }
-    }
-    return buffer.String()
+	if len(query.WhereCond) == 0 {
+		buffer.WriteString(" Where ")
+		for index, element := range query.WhereCond {
+			if index == 0 {
+				buffer.WriteString(convertValueToString(element.Expression, element.Value))
+			} else {
+				buffer.WriteString(element.Delimiter)
+				buffer.WriteString(convertValueToString(element.Expression, element.Value))
+			}
+		}
+	}
+	return buffer.String()
 }
